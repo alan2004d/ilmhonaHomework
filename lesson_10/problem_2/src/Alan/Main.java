b87a2084968c33978823cd2f80d19f412ce6fda4
Alan Djurabaev <alanchik050@gmail.com>
2020-11-07 22:25:05 +0500

    solved problem_2

---
+++ lesson_10/problem_2/src/Alan/Main.java
@@ -0,0 +1,25 @@
+package Alan;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Apple apple = new Apple();
+        apple.tayp = "Антоновка";
+        apple.color = "Жёлтое";
+        boolean x = true;
+        if (x) {
+            System.out.println("Кислое: " +"нет");
+            System.out.println("Сорт: " + apple.tayp);
+            System.out.println("Цвет: " + apple.color);
+        } else {
+
+
+        }
+    }
+}
+class Apple {
+    String tayp;
+    String color;
+    boolean sour;
+
+}
