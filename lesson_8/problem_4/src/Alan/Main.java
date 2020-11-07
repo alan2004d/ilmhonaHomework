6286d2a9de66a7d7468b24be3f794179258b3b75
Alan Djurabaev <alanchik050@gmail.com>
2020-11-07 19:48:33 +0500

    solved problem_4

---
+++ lesson_8/problem_4/src/Alan/Main.java
@@ -0,0 +1,26 @@
+package Alan;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = {6, 3, 9, 5, -9, -2, 1, 5, 6, 3};
+        arraySum(array);
+        int arrayPlus = arraySum(array);
+        System.out.println(arrayPlus);
+
+    }
+
+    public static int arraySum(int[] array) {
+
+        int arrayPlus = array[0] + array[1] + array[2] + array[3] + array[4] + array[5] + array[6] +
+                array[7] + array[8] + array[9];
+        {
+
+
+            {
+
+                return arrayPlus;
+
+            }
+        }
+    }}
