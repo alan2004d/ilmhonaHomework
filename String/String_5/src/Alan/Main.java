fd89a8cd2ecf16d4f0692f69279587e577e354c3
Alan Djurabaev <alanchik050@gmail.com>
2020-11-19 21:28:54 +0500

    Solved string_5

---
+++ String/String_5/src/Alan/Main.java
@@ -0,0 +1,11 @@
+package Alan;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String string = "This is word and this word is perfect";
+        string =string.replaceAll("word","letter");
+        System.out.println(string);
+
+    }
+}
