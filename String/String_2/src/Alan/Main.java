eb51ae8498557d1687e51a21092b31ac04a730fe
Alan Djurabaev <alanchik050@gmail.com>
2020-11-19 19:54:51 +0500

    Solved string_2

---
+++ String/String_2/src/Alan/Main.java
@@ -0,0 +1,17 @@
+package Alan;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String string = "nomo loco";
+        char lastSymbol = string.charAt(string.length() - 1);
+        System.out.println("Последний символ: " + lastSymbol);
+        int i;
+        for (i = 0; i < string.length(); i++) {
+
+        if (string.charAt(i) == lastSymbol)
+        
+            System.out.println(i + 1 + " ");
+        }
+    }
+}
