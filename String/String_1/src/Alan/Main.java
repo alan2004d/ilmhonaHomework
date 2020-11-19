3d387e3fe5ef29d073dc2444c4fd590c575c935e
Alan Djurabaev <alanchik050@gmail.com>
2020-11-19 21:09:32 +0500

    Solved string_1

--- String/String_1/src/Alan/Main.java
+++ String/String_1/src/Alan/Main.java
@@ -3,16 +3,17 @@ package Alan;
 public class Main {
 
     public static void main(String[] args) {
-	String string = "Hello world, I like Java";
-	if (string.length() > 5){
-	    String firstPart = string.substring(0, 3);
-	    String lastPart = string.substring(string.length() - 3);
-        System.out.println("Первые три символа: " + firstPart);
-        System.out.println("Последние три символа: " + lastPart);
-    }else{
-	    for (int i=0; i<string.length(); i++){
-            System.out.println(string.substring(0,1));
+        String string = "Hello world, I like Java";
+        if (string.length() > 5) {
+            String firstPart = string.substring(0, 3);
+            String lastPart = string.substring(string.length() - 3);
+            System.out.println("Первые три символа: " + firstPart);
+            System.out.println("Последние три символа: " + lastPart);
+        } else {
+            for (int i = 0; i < string.length(); i++) {
+                System.out.println(string.substring(0, 1));
+
+            }
         }
-    }
     }
 }
